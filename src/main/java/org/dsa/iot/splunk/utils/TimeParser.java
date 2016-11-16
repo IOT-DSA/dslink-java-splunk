e92de0c2a12c13e2c5289ce0e3ddcc90c07eb7e4
Aaron <aaron_hansen@yahoo.com>
2016-11-16 15:54:16 -0800

    0.0.2 - Update sdk dependency and use the new time parsing in the sdk.

--- src/main/java/org/dsa/iot/splunk/utils/TimeParser.java
+++ src/main/java/org/dsa/iot/splunk/utils/TimeParser.java
@@ -1,55 +1,18 @@
 package org.dsa.iot.splunk.utils;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
+import org.dsa.iot.dslink.util.TimeUtils;
 
 /**
  * @author Samuel Grenier
  */
 public class TimeParser {
 
-    private static final ThreadLocal<DateFormat> FORMAT_TIME_ZONE;
-    private static final ThreadLocal<DateFormat> FORMAT;
-
     public static long parse(String time) {
-        try {
-            if (time.matches(".+[+|-]\\d+:\\d+")) {
-                StringBuilder b = new StringBuilder(time);
-                b.deleteCharAt(time.lastIndexOf(":"));
-                time = b.toString();
-                return FORMAT_TIME_ZONE.get().parse(time).getTime();
-            } else {
-                return FORMAT.get().parse(time).getTime();
-            }
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
+        return TimeUtils.decode(time);
     }
 
     public static String parse(long time) {
-        return FORMAT.get().format(new Date(time)) + "-00:00";
+        return TimeUtils.encode(time, true).toString();
     }
 
-    static {
-        FORMAT_TIME_ZONE = new ThreadLocal<DateFormat>() {
-            @Override
-            protected DateFormat initialValue() {
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
-                sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-                return sdf;
-            }
-        };
-
-        FORMAT = new ThreadLocal<DateFormat>() {
-            @Override
-            protected DateFormat initialValue() {
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
-                sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
-                return sdf;
-            }
-        };
-    }
 }
